b8b5ada3c5590019ad830094c4ca307c1ab09555
meganlynnesm <meganmackin0@gmail.com>
2026-08-05 22:55:45 -0400

    main.js file updates

--- main.js
+++ main.js
@@ -6,3 +6,72 @@ var map = new maplibregl.Map({
 });
 
 map.addControl(new maplibregl.NavigationControl());
+
+map.on('load', () => {
+
+  // ---- benches ----
+  map.addSource('benches', {
+    type: 'geojson',
+    data: 'benches.geojson'
+  });
+
+  map.addLayer({
+    id: 'benches-layer',
+    type: 'circle',
+    source: 'benches',
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#CFD11A',        // Lemon Lime
+      'circle-stroke-width': 1.5,
+      'circle-stroke-color': '#1C0103'  // Coffee Bean
+    }
+  });
+
+  // ---- cafes ----
+  map.addSource('cafes', {
+    type: 'geojson',
+    data: 'cafes.geojson'
+  });
+
+  map.addLayer({
+    id: 'cafes-layer',
+    type: 'circle',
+    source: 'cafes',
+    paint: {
+      'circle-radius': 7,
+      'circle-color': '#18486B',        // Yale Blue
+      'circle-stroke-width': 1.5,
+      'circle-stroke-color': '#F3EDDE'  // Old Lace
+    }
+  });
+
+  map.on('click', 'benches-layer', (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const props = e.features[0].properties;
+
+    new maplibregl.Popup()
+      .setLngLat(coordinates)
+      .setHTML('<strong>' + props.bench_id + '</strong><br>source: ' + props.source)
+      .addTo(map);
+  });
+
+  map.on('click', 'cafes-layer', (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const props = e.features[0].properties;
+
+    new maplibregl.Popup()
+      .setLngLat(coordinates)
+      .setHTML('<strong>' + props.name + '</strong><br>opens ' + props.opens)
+      .addTo(map);
+  });
+
+  ['benches-layer', 'cafes-layer'].forEach((layerId) => {
+    map.on('mouseenter', layerId, () => {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', layerId, () => {
+      map.getCanvas().style.cursor = '';
+    });
+  });
+
+});
